d50d5bda3420c2bd2b0639a0e6fdff44b90c664d
darielgaizta <darielgaz@gmail.com>
2024-02-08 14:10:48 +0700

    fix and refactor CalculatorTestCase

--- src/test/java/com/darielgaizta/essaytask/CalculatorTestCase.java
+++ src/test/java/com/darielgaizta/essaytask/CalculatorTestCase.java
@@ -6,14 +6,14 @@ import org.testng.annotations.Test;
 import java.util.Random;
 
 public class CalculatorTestCase {
-    @Test
+    @Test(description = "0/0 should return NaN.")
     public void zeroes() {
         /* Test: 0/0 should return NaN */
         Calculator calculator = new Calculator();
         Assert.assertEquals(calculator.count(0, 0, 0, 0), Double.NaN);
     }
 
-    @Test
+    @Test(description = "Division between positive numbers (+/+) should return a positive (+) number")
     public void allPositives() {
         /* Test: Division between positive numbers (+/+) should return a positive (+) number */
         Calculator calculator = new Calculator();
@@ -28,7 +28,7 @@ public class CalculatorTestCase {
         Assert.assertEquals(actual, expected, 0.00001);
     }
 
-    @Test
+    @Test(description = "Division between negative numbers (-/-) should return a positive (+) number")
     public void allNegatives() {
         /* Test: Division between negative numbers (-/-) should return a positive (+) number */
         Calculator calculator = new Calculator();
@@ -43,97 +43,118 @@ public class CalculatorTestCase {
         Assert.assertEquals(actual, expected, 0.00001);
     }
 
-    @Test
-    public void largeNumbers() {
-        /* Test: Division between large numbers (MAX_VALUE & POSITIVE_INFINITY) should return NaN */
+    @Test(description = "Division between large positive numbers (MAX_VALUE) should return 1")
+    public void largePositiveNumbers() {
+        /* Test: Division between large positive numbers (MAX_VALUE) should return 1
+        * This method will test if the function can handle a large positive number like MAX_VALUE. */
         Calculator calculator = new Calculator();
         Assert.assertEquals(
-                calculator.count(Double.MAX_VALUE, Double.MAX_VALUE, Double.MAX_VALUE, Double.MAX_VALUE),
-                Double.NaN
+                calculator.count(Double.MAX_VALUE, 0, Double.MAX_VALUE, 0),
+                1
         );
+    }
+
+    @Test(description = "Division between POSITIVE_INFINITY-s should return NaN")
+    public void positiveInfinityNumbers() {
+        /* Test: Division between POSITIVE_INFINITY-s should return NaN */
+        Calculator calculator = new Calculator();
         Assert.assertEquals(
                 calculator.count(Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY),
                 Double.NaN
         );
     }
 
-    @Test
-    public void smallNumbers() {
-        /* Test: Division between NEGATIVE_INFINITY-s should return NaN and MIN_VALUE should return 1 */
+    @Test(description = "Division between small positive numbers (MIN_VALUE) should return 1")
+    public void smallPositiveNumbers() {
+        /* Test: Division between small positive numbers (MIN_VALUE) should return 1
+        * This method will test if the function can handle a small positive number like MIN_VALUE. */
         Calculator calculator =  new Calculator();
         Assert.assertEquals(
-                calculator.count(Double.MIN_VALUE, Double.MIN_VALUE, Double.MIN_VALUE, Double.MIN_VALUE),
+                calculator.count(Double.MIN_VALUE, 0, Double.MIN_VALUE, 0),
                 1
         );
+    }
+
+    @Test(description = "Division between large negative numbers (-MAX_VALUE) should return 1")
+    public void largeNegativeNumbers() {
+        /* Test: Division between large negative numbers (-MAX_VALUE) should return 1 */
+        Calculator calculator = new Calculator();
+        Assert.assertEquals(
+                calculator.count(-Double.MAX_VALUE, 0, -Double.MAX_VALUE, 0),
+                1
+        );
+    }
+
+    @Test(description = "Division between NEGATIVE_INFINITY-s should return NaN")
+    public void negativeInfinityNumbers() {
+        /* Test: Division between NEGATIVE_INFINITY-s should return NaN */
+        Calculator calculator = new Calculator();
         Assert.assertEquals(
                 calculator.count(Double.NEGATIVE_INFINITY, Double.NEGATIVE_INFINITY, Double.NEGATIVE_INFINITY, Double.NEGATIVE_INFINITY),
                 Double.NaN
         );
     }
 
-    @Test
-    public void mixedRandomNumbers() {
-        /* Test: Division between positive and negative (+/- or -/+) should return a negative number */
+    @Test(description = "Positive numbers divided by negative numbers should return a negative number")
+    public void positivesDividedByNegatives() {
+        /* Test: Positive numbers divided by negative numbers (+/-) should return a negative number (-) */
         Random random = new Random();
         Calculator calculator = new Calculator();
 
         double posValues = random.nextDouble();
         double negValues = random.nextDouble() * -1;
-        double result1 = calculator.count(posValues, posValues, negValues, negValues);
-        double result2 = calculator.count(negValues, negValues, posValues, posValues);
-
-        Assert.assertTrue(result1 < 0, "Result 1 is negative.");
-        Assert.assertTrue(result2 < 0, "Result 2 is negative.");
+        double result = calculator.count(posValues, posValues, negValues, negValues);
+        Assert.assertTrue(result < 0);
     }
 
-    @Test
-    public void anyWithZeroDivision() {
-        /* Test: Any number divided by zero should return Infinite */
+    @Test(description = "Negative numbers divided by positive numbers should return a negative number")
+    public void negativesDividedByPositives() {
+        /* Test: Negative numbers divided by positive numbers (-/+) should return a negative number (-) */
+        Random random = new Random();
+        Calculator calculator = new Calculator();
+
+        double posValues = random.nextDouble();
+        double negValues = random.nextDouble() * -1;
+        double result = calculator.count(negValues, negValues, posValues, posValues);
+        Assert.assertTrue(result < 0);
+    }
+
+    @Test(description = "Positive numbers divided by zero should return Infinite")
+    public void positivesWithZeroDivision() {
+        /* Test: Positive numbers divided by zero should return Infinite */
         Calculator calculator = new Calculator();
         Assert.assertEquals(calculator.count(1, 1, 0, 0), Double.POSITIVE_INFINITY);
+    }
+
+    @Test(description = "Negative numbers divided by zero should return negative Infinite")
+    public void negativeWithZeroDivision() {
+        /* Test: Negative numbers divided by zero should return negative Infinite */
+        Calculator calculator = new Calculator();
         Assert.assertEquals(calculator.count(-1, -1, 0, 0), Double.NEGATIVE_INFINITY);
     }
 
-    @Test
-    public void largeNumberAsResult() {
-        /* Test: Function should be able to return a large number */
+    @Test(description = "Function should be able to return a large positive number (MAX_VALUE)")
+    public void largePositiveNumberAsResult() {
+        /* Test: Function should be able to return a large positive number (MAX_VALUE) */
         Calculator calculator = new Calculator();
-        Assert.assertNotEquals(calculator.count(Double.MAX_VALUE, 1, 1, 0), Double.POSITIVE_INFINITY);
+        Assert.assertEquals(calculator.count(Double.MAX_VALUE, 0, 1, 0), Double.MAX_VALUE);
     }
 
-    @Test
-    public void smallNumberAsResult() {
-        /* Test: Function should be able to return a small number */
+    @Test(description = "Function should be able to return a small positive number (MIN_VALUE)")
+    public void smallPositiveNumberAsResult() {
+        /* Test: Function should be able to return a small positive number (MIN_VALUE) */
         Calculator calculator = new Calculator();
-        Assert.assertNotEquals(calculator.count(Double.MAX_VALUE, 1, -1, 0), Double.NEGATIVE_INFINITY);
+        Assert.assertEquals(calculator.count(Double.MIN_VALUE, 0, 1, 0), Double.MIN_VALUE);
     }
 
-    @Test
-    public void randomNumbers() {
+    @Test(description = "Function should be able to return a large negative number (-MAX_VALUE)")
+    public void largeNegativeNumberAsResult() {
+        /* Test: Function should be able to return a large negative number (-MAX_VALUE) */
         Calculator calculator = new Calculator();
-
-        double a = Math.random();
-        double b = Math.random();
-        double c = Math.random();
-        double d = Math.random();
-
-        double result = calculator.count(a, b, c, d);
-
-        if (c + d == 0) {
-            if (a + b == 0) {
-                /* Test: 0/0 should return NaN */
-                Assert.assertEquals(result, Double.NaN);
-            } else {
-                /* Test: Any random number divided by zero should return infinite */
-                Assert.assertEquals(result, Double.POSITIVE_INFINITY);
-            }
-        } else {
-            /* Test: Function should return any positive number */
-            Assert.assertTrue(result > 0);
-        }
+        Assert.assertEquals(calculator.count(Double.MAX_VALUE, 0, -1, 0), -Double.MAX_VALUE);
     }
 
-    @Test
+    @Test(description = "Function should return a precise number")
     public void precisionCheck() {
         /* Test: Function should return a precise number */
         Calculator calculator = new Calculator();
@@ -148,7 +169,7 @@ public class CalculatorTestCase {
         Assert.assertEquals(actual, expected, 0.00000001);
     }
 
-    @Test
+    @Test(description = "Function should apply a commutative law")
     public void commutativeCheck() {
         /* Test: Function should apply a commutative law */
         Calculator calculator = new Calculator();
